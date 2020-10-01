4c70fdc5c8c862c7dd18c75684d26c377d8d9cd8
kurwitz3 <kurwitz3@aol.com>
2020-10-01 18:48:39 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, "secret"
+      end
+      get '/' do 
+        erb :index
+      end 
+      post '/checkout' do 
+        @purchase = params[:item]
+        @session = session 
+        @session.merge! params
+        erb :show
+      end
 end
